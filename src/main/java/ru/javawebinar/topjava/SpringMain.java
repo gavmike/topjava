1ff2de03e5d361e3cdfda51c66d670f494b22035
mike <gavrilin.mike@gmail.com>
2020-02-15 22:09:42 +0300

    impl InMemoryUserRepo

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,17 +4,35 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+          //  System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+          //  AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+           // adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            InMemoryUserRepository inMemoryUserRepository = (InMemoryUserRepository)new InMemoryUserRepository();
+            System.out.println((List<User>) inMemoryUserRepository.getAll());
+            System.out.println(inMemoryUserRepository.get(1));
+            System.out.println(inMemoryUserRepository.get(2));
+            //inMemoryUserRepository.delete(2);
+
+            inMemoryUserRepository.save(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            User newUser = new User(3, "userName3", "email@mail.ru3", "password", Role.ROLE_ADMIN);
+            inMemoryUserRepository.save(newUser);
+            for (User users:inMemoryUserRepository.getAll()) {
+                System.out.println(users.getId());
+                System.out.println(users.getName());
+            }
+                
+            }
         }
-    }
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,75 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
+
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private Map<Integer,User> usersStorage = new ConcurrentHashMap<>();
+    AtomicInteger counter = new AtomicInteger();
+    {
+        User user1 = new User(null, "userName1", "email1@mail.ru", "password1", Role.ROLE_ADMIN);
+        User user2 = new User(null, "userName2", "email2@mail.ru", "password2", Role.ROLE_USER);
+       int i1 = counter.incrementAndGet();
+        user1.setId(i1);
+        usersStorage.put(user1.getId(),user1);
+        int i2 = counter.incrementAndGet();
+        user2.setId(i2);
+        usersStorage.put(user2.getId(),user2);
+
+            }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return usersStorage.remove(id)!=null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if(user.isNew()){
+         int i = counter.incrementAndGet();
+         user.setId(i);
+         usersStorage.put(user.getId(),user);
+         return user;
+        }
+        else return usersStorage.computeIfPresent(user.getId(),(id,oldUser)->user) ;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return usersStorage.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return  usersStorage.values().stream().collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        Iterator<Map.Entry<Integer,User>> iterator = usersStorage.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mapElement = (Map.Entry) iterator.next();
+            User user = (User)  mapElement.getValue();
+            if(user.getEmail().equals(email)) return usersStorage.get(user.getId());
+        }
         return null;
     }
 }
