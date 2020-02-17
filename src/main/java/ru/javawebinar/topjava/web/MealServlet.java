392519377b92e75ccf4ae49eb726fa75f9cc125b
mike <gavrilin.mike@gmail.com>
2020-02-17 16:02:21 +0300

    work witch autorized

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -41,11 +41,13 @@ public class MealServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         String id = request.getParameter("id");
 
+
         Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("calories")),
                         SecurityUtil.authUserId()
+
                 );
 
         log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -3,12 +3,18 @@ package ru.javawebinar.topjava.web;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+ private static   int Id;
 
+public static void setAuthUserId(int userId){
+    Id =userId;
+
+}
     public static int authUserId() {
-        return 1;
+        return Id;
     }
 
     public static int authUserCaloriesPerDay() {
         return DEFAULT_CALORIES_PER_DAY;
     }
+
 }
