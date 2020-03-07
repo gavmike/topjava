45d692a332837327976049f88722870576972591
mike <gavrilin.mike@gmail.com>
2020-03-07 23:15:00 +0300

    clear code

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -18,7 +18,7 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     int delete (@Param("id") int id,@Param("userId") int userId);
 
     Meal getByIdAndUserId(int id, int userId);
-   // List<Meal> getAllByDateIsEndingWithAndUserId(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
+
     @Query( "SELECT m FROM Meal m " +
             "WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDateTime") LocalDateTime startDateTime,@Param("endDateTime") LocalDateTime endDateTime,@Param("userId") int userId);
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -22,43 +22,31 @@ public class DataJpaMealRepository implements MealRepository {
     @Override
     @Transactional
     public Meal save(Meal meal, int userId) {
-
-        if(!meal.isNew()  && get(meal.getId(),userId)==null){
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
             return null;
         }
-
-
-           meal.setUser(userRepository.getOne(userId));
-           return crudRepository.save(meal);
-
+        meal.setUser(userRepository.getOne(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
         return crudRepository.delete(id, userId) != 0;
-
     }
 
     @Override
     public Meal get(int id, int userId) {
-
         return crudRepository.getByIdAndUserId(id, userId);
-
     }
 
     @Override
     public List<Meal> getAll(int userId) {
         return crudRepository.findAllByUserId(userId).stream().sorted(((meal, t1) -> t1.getId().compareTo(meal.getId())))
                 .collect(Collectors.toList());
-        /*return   crudRepository.findAll();*//*.stream().sorted(((meal, t1) -> meal.getId().compareTo(t1.getId())))
-              .collect(Collectors.toList());
-*/
-
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        // return crudRepository.getAllByDateIsEndingWithAndUserId(startDateTime, endDateTime, userId);
         return crudRepository.getBetween(startDateTime, endDateTime, userId);
     }
 }
