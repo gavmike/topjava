7cc9fbd2f85fae26c3ef0f3a5f8f80b7a7eb89f8
mike <gavrilin.mike@gmail.com>
2020-03-09 22:01:40 +0300

    part optional

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,7 +17,9 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(new String []{"spring/spring-app.xml", "spring/inmemory.xml"},false)) {
+            appCtx.getEnvironment().setActiveProfiles(Profiles.DATAJPA,Profiles.HSQL_DB);
+            appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealTest.java
@@ -4,6 +4,7 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.service.MealServiceTest;
 
-@ActiveProfiles({Profiles.POSTGRES_DB,Profiles.DATAJPA})
+//@ActiveProfiles({Profiles.HSQL_DB,Profiles.DATAJPA})
+@ActiveProfiles( Profiles.DATAJPA)
 public class DataJpaMealTest extends MealServiceTest {
 }
